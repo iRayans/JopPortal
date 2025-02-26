d2bbcca3516d6a9f79252adcf1dd0c8fcba2fcdb
rharbi <iirharbi@gmail.com>
2025-02-26 12:37:29 +0300

    Create UerType Entity

---
+++ src/main/java/com/rayan/jobportal/entity/UserType.java
@@ -0,0 +1,60 @@
+package com.rayan.jobportal.entity;
+
+import jakarta.persistence.*;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users_type")
+public class UserType {
+
+    public UserType(int userTypeId, String userTypeName, List<User> users) {
+        this.userTypeId = userTypeId;
+        this.userTypeName = userTypeName;
+        this.users = users;
+    }
+
+    public UserType() {
+    }
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int userTypeId;
+
+    private String userTypeName;
+
+    @OneToMany(targetEntity = User.class, mappedBy = "userTypeId", cascade = CascadeType.ALL)
+    private List<User> users;
+
+    @Override
+    public String toString() {
+        return "UserType{" +
+                "userTypeName='" + userTypeName + '\'' +
+                ", userTypeId=" + userTypeId +
+                '}';
+    }
+
+    public int getUserTypeId() {
+        return userTypeId;
+    }
+
+    public void setUserTypeId(int userTypeId) {
+        this.userTypeId = userTypeId;
+    }
+
+    public String getUserTypeName() {
+        return userTypeName;
+    }
+
+    public void setUserTypeName(String userTypeName) {
+        this.userTypeName = userTypeName;
+    }
+
+    public List<User> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<User> users) {
+        this.users = users;
+    }
+}
