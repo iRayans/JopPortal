4c5fc299952a99b1062a169720da041ab59dbb5d
rharbi <iirharbi@gmail.com>
2025-02-28 02:58:18 +0300

    Add create user method

--- src/main/java/com/rayan/jobportal/controller/UserController.java
+++ src/main/java/com/rayan/jobportal/controller/UserController.java
@@ -4,10 +4,12 @@ import com.rayan.jobportal.entity.User;
 import com.rayan.jobportal.entity.UserType;
 import com.rayan.jobportal.service.UserService;
 import com.rayan.jobportal.service.UserTypeService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -15,10 +17,12 @@ import java.util.List;
 public class UserController {
 
     private final UserTypeService userTypeService;
+    private final UserService userService;
 
     @Autowired
-    public UserController(UserTypeService userTypeService) {
+    public UserController(UserTypeService userTypeService, UserService userService) {
         this.userTypeService = userTypeService;
+        this.userService = userService;
     }
 
     @GetMapping("/register")
@@ -27,5 +31,12 @@ public class UserController {
         model.addAttribute("getAllTypes", userTypes);
         model.addAttribute("user", new User());
         return "register";
+    }
+
+    @PostMapping("/register/new")
+    public String register(@Valid User user ) {
+        List<UserType> userTypes = userTypeService.getAll();
+        userService.addNewUser(user);
+        return "dashboard";
     }
 }
--- src/main/java/com/rayan/jobportal/service/UserService.java
+++ src/main/java/com/rayan/jobportal/service/UserService.java
@@ -1,7 +1,21 @@
 package com.rayan.jobportal.service;
 
+import com.rayan.jobportal.entity.User;
+import com.rayan.jobportal.repository.UserRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.Date;
 
 @Service
 public class UserService {
+    private final UserRepository userRepository;
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    public User addNewUser(User user) {
+        user.setActive(true);
+        user.setregistrationDate(new Date(System.currentTimeMillis()));
+        return userRepository.save(user);
+    }
 }
